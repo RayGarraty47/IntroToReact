7d1f4509590c7b5ee7447736c8286ca4e770fbca
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-03-08 10:38:12 -0500

    core react concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,52 @@
+import { createRoot } from "react-dom/client";
 import React from "react";
-import { createRoot } from "react-dom";
-
-// Into the child element
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h1", {}, props.animal),
-    React.createElement("h1", {}, props.breed),
-  ]);
-};
+import SearchParams from "./SearchParams";
 
 // Passing the following parent properties
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Doink",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-  ]);
+  return (
+  <div> 
+    <h1>Adopt Me!</h1>
+    <SearchParams />
+  </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
+
+
+
+
+
+const A = "A";
+let F;
+
+function doStuff(B) {
+  console.log(B); // in scope
+  const C = "C";
+  let H = "H";
+  if (1 + 1 === 2) {
+    const D = "D";
+    H = "something else"; // in scope
+  }
+  console.log(D); //out of scope D was declared in if statement block
+  console.log(H); // in scope H was declared in same funciton block
+  F = "F"; 
+}
+
+let E = 0;
+while (E < 3) {
+  E++;
+  console.log(A); // in scope
+  const G = "G";
+}
+console.log(E); // in scope 
+console.log(G); // out of scope
+
+doStuff("B");
+console.log(B); // out of scope
+console.log(C); // out of scope
+console.log(F); // in scope
